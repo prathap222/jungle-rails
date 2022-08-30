13c27a5949c18058dbb29aaa013f728b6a6f071b
Prathap <prathap.sims@gmail.com>
2022-08-30 06:44:37 +0000

    Add Rspec test for user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_30_000946) do
+ActiveRecord::Schema.define(version: 2022_08_30_055006) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -63,6 +63,8 @@ ActiveRecord::Schema.define(version: 2022_08_30_000946) do
     t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "fname"
+    t.string "lname"
   end
 
   add_foreign_key "line_items", "orders"
